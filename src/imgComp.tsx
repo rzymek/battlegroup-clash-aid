270a4b18509c46295ac5a4bd6602553d7af0cc15
rzymek <krzysztof.rzymkowski@gmail.com>
2026-04-05 21:30:47 +0200

    src/imgComp.tsx:10:76 - error TS2698: Spread types may only be created from object types.

--- src/imgComp.tsx
+++ src/imgComp.tsx
@@ -1,11 +1,11 @@
-import {ImgHTMLAttributes} from "preact"
+import {CSSProperties, ImgHTMLAttributes} from "preact"
 
 export function imgComp(src: string) {
   return () => <Img src={src}/>
 }
 
-export function Img(props: ImgHTMLAttributes) {
-  const {style={}, ...rest} = props;
+export function Img(props: Omit<ImgHTMLAttributes, 'style'> & { style?: CSSProperties }) {
+  const {style = {}, ...rest} = props;
   return <img alt=""
               style={{height: '15mm', width: 'auto', objectFit: 'contain', ...style}}
               {...rest}
