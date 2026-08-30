77773696ec9a8b3f533265b29b48a4c981903713
rzymek <rzymek@gmail.com>
2026-08-30 16:16:21 +0200

    indirect: all / values bolding

--- src/DRMExplained.tsx
+++ src/DRMExplained.tsx
@@ -1,6 +1,17 @@
 import {calculateDRM, DRMDef, DRMState, LabelsFor} from "./calculateDRM.tsx";
 import {BoldifyValue} from "./BoldifyValue.tsx";
 
+const values = {
+  fst: 'FST',
+  recce: 'Recce',
+  shellScrapes: 'Shell Scrape',
+  digging: 'Digging',
+  lightWood: "Light Wood",
+  denseWood: "Dense Wood",
+  lightUrban: "Light Urban",
+  urban: "Urban",
+} as const;
+
 export function DRMExplained<T extends { drm: DRMState }>(props: {
   state: T,
   drm: DRMDef<T>
@@ -16,20 +27,17 @@ export function DRMExplained<T extends { drm: DRMState }>(props: {
     </tr>
     </thead>
     <tbody>
-    {drm.reasons.map((it, idx) => <tr key={idx}>
-      <td>
-        <div><BoldifyValue value={props.state.drm[it.reason]} values={{
-          lightWood: "Light Wood",
-          denseWood: "Dense Wood",
-          lightUrban: "Light Urban",
-          urban: "Urban",
-        }}>
-          {props.reasonLabels[it.reason] ?? it.reason}
-        </BoldifyValue></div>
-        <div>{it.note}</div>
-      </td>
-      <th>{it.modifier > 0 && '+'}{it.modifier}</th>
-    </tr>)}
+    {drm.reasons.map((it, idx) => {
+      return <tr key={idx}>
+        <td>
+          <div><BoldifyValue value={props.state.drm[it.reason]} values={values}>
+            {props.reasonLabels[it.reason] ?? it.reason}
+          </BoldifyValue></div>
+          <div>{it.note}</div>
+        </td>
+        <th>{it.modifier > 0 && '+'}{it.modifier}</th>
+      </tr>;
+    })}
     </tbody>
   </table>
 }
--- src/indirect/ReasonLabels.tsx
+++ src/indirect/ReasonLabels.tsx
@@ -5,12 +5,12 @@ export const reasonLabels: LabelsFor<typeof indirectDRM> = {
   ew_uas_coordination: `UAS coordination: UAS with LoS to target grants +4 to this Indirect Fire (i.e. additional +2). 1A`,
   ew_interference: "EW Interference: Opponent has -2 DRM on Artillery result. 3B",
   ew_triangulation: "EW Triangulation: Gain +2 DRM on Artillery result. 3A",
-  losSupport_other: 'Friendly Recce / FST with LoS to target',
+  losSupport_other: 'Friendly [Recce] / [FST] with LoS to target',
   losSupport_uas: 'UAS with LoS to target',
   target_footInTerrain: 'Target is Foot FE in [Light Wood]*/[Light Urban] / [Dense Wood]*/[Urban]',
   target_marker: 'Value of played Target marker',
   target_moved: 'Target has Moved marker**',
-  target_shellScrapes: 'Target has Digging / Shell Scrape marker',
+  target_shellScrapes: 'Target has [Digging] / [Shell Scrape] marker',
   target_tracked: 'Target is Tracked FE Type',
   fpv_jamming: 'FPV jamming: Opponent FPV attack: Roll 1D6. Apply that # as a -DRM on the result. 4B'
 };
